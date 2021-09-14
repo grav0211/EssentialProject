d924ebc208ec45f4deaffc4381ed0dfbabccb438
Alexandre Gravelle <agrav065@gmail.com>
2021-09-14 08:18:32 -0400

    Move ErrorView to separate file

---
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Alexandre Gravelle on 2021-09-14.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
