f871ddb37045e3780b70f4b5f9df3ec6f3e2ce83
grav0211 <agrav065@gmail.com>
2022-10-07 06:11:33 -0400

    Localize image comments title

---
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCommentsLocalizationTests.swift
+//  EssentialFeedTests
+//
+//  Created by Alexandre Gravelle on 2022-10-07.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsLocalizationTests: XCTestCase {
+    
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+    
+}
