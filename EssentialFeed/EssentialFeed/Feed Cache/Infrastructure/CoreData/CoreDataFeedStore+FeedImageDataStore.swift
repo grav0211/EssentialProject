4a67737baed0b82f5215a472f0edbb317c157564
grav0211 <agrav065@gmail.com>
2022-10-11 07:26:00 -0400

    Add helper to find image data for a given URL

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -22,7 +22,7 @@ extension CoreDataFeedStore: FeedImageDataStore {
     
     public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
         perform { context in
-            completion(Result { try ManagedFeedImage.first(with: url, in: context)?.data })
+            completion(Result { try ManagedFeedImage.data(with: url, in: context) })
         }
     }
     
